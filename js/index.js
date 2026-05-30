e743a34ab3d14692a75fab9da4f9e4bcf47e4a85
Rafael Cortez <rafael.ecortez@gmail.com>
2026-05-29 22:10:02 -0300

    chore(index.js): initialize app by loading basic HTML on DOM

---
+++ js/index.js
@@ -0,0 +1,6 @@
+import {loadBasicHTML} from "./modules/basic-html/basic-html.js";
+
+document.addEventListener("DOMContentLoaded", async () => {
+    // creation of basic components in HTML
+    loadBasicHTML();
+});

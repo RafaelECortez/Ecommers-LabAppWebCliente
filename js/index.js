e0e167d2d722498b6ee9125282f4efa7e77f8921
NoeliaESanabria <noelia.sanabria@istea.com.ar>
2026-06-01 18:06:17 -0300

    feat: conectar función base mostrarModalDetalle al index.js

--- js/index.js
+++ js/index.js
@@ -1,6 +1,13 @@
-import {loadBasicHTML} from "./modules/basic-html/basic-html.js";
+import { loadBasicHTML } from "./modules/basic-html/basic-html.js";
+// 1. Importamos la función mostrarModalDetalle
+import { mostrarModalDetalle } from "./modules/ui.components/producto.modal.js";
 
 document.addEventListener("DOMContentLoaded", async () => {
-    // creation of basic components in HTML
-    loadBasicHTML();
+  // creation of basic components in HTML
+  loadBasicHTML();
+
+  // 2. Ejecutamos la función de prueba con un producto falso
+  const productoPrueba = { nombre: "Zapatillas de prueba" };
+  mostrarModalDetalle(productoPrueba);
 });
+
---
+++ js/modules/ui.components/producto.modal.js
@@ -0,0 +1,4 @@
+export const mostrarModalDetalle = (producto) => {
+    // Acá vamos a armar el HTML del modal más adelante
+    console.log("¡Función mostrarModalDetalle conectada! Producto recibido:", producto.nombre);
+};
